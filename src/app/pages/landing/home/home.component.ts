6ac8857c20b90eeb8de840200019b0840a645abc
Naim Mesias <naimmesias1968@hotmail.com>
2025-04-25 17:20:12 -0500

    feat(home): add toolbar with menu and style improvements
    Introduce a new toolbar with a dropdown menu for mousepads and enhance styling for better user experience. This includes adding Angular CDK components for menu functionality and updating SCSS for consistent UI elements.

--- src/app/pages/landing/home/home.component.ts
+++ src/app/pages/landing/home/home.component.ts
@@ -2,11 +2,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../material.module';
-
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule,
+    CdkMenuTrigger,
+    CdkMenu,
+    CdkMenuItem,
+    RouterModule,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,7 +26,7 @@ export class HomeComponent {
       name: 'Featured Product 1',
       price: 99.99,
       image: 'assets/images/products/product1.jpg',
-      description: 'Amazing product description'
+      description: 'Amazing product description',
     },
     // Add more featured products as needed
   ];
